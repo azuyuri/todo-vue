be862ec31653461258488895d5619dc31497ef65
azuyuri <tamondojo@gmail.com>
2018-11-04 18:55:00 +0900

    feat:削除ボタン追加

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <input type="text" class="todo-input" placeholder="What needs to be done">
-    Todo list goes here !
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      <div>
+      {{ todo.title }}
+      </div>
+      <div class="remove-item">
+        &times;
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +17,36 @@ export default {
   name: 'todo-list',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newTodo: '',
+      idForTodo: 3,
+      todos: [
+        {
+          'id': 1,
+          'title': 'Finish Vue Screencast',
+          'completed': false,
+        },
+        {
+          'id': 2,
+          'title': 'Take over world',
+          'completed': false,
+        },
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return
+      }
+
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      })
+
+      this.newTodo = ''
+      this.idForTodo++
     }
   }
 }
@@ -18,5 +54,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+
+.todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover {
+      color: black;
+    }
+  }
 
 </style>
