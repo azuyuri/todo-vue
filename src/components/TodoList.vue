5f6bc105f2803f7487a959c4efb0f4e77c279ccf
azuyuri <tamondojo@gmail.com>
2018-11-04 20:18:29 +0900

    feat:removeTodoメソッド追加

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
-    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
       <div>
       {{ todo.title }}
       </div>
-      <div class="remove-item">
+      <div class="remove-item" @click="removeTodo(index)">
         &times;
       </div>
     </div>
@@ -47,6 +47,9 @@ export default {
 
       this.newTodo = ''
       this.idForTodo++
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
     }
   }
 }
