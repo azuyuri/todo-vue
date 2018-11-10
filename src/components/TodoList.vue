cb6fc4a8d75bd71eb990f625ab681c344834668b
azuyuri <tamondojo@gmail.com>
2018-11-10 10:52:11 +0900

    refactor: Remove unused code from TodoList.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -22,17 +22,6 @@
       @removedTodo="removeTodo"
       @finishedEdit="finishedEdit"
     >
-      <!-- <div class="todo-item-left">
-
-        <input type="checkbox" v-model="todo.completed">
-
-        <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed : todo.completed }">{{ todo.title }}</div>
-
-        <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
-          </div>
-        <div class="remove-item" @click="removeTodo(index)">
-          &times;
-        </div> -->
     </todo-item>
   </transition-group>
 
