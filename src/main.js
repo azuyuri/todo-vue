912f35c353dfffa59cc398dcfb8f74ee00abd636
azuyuri <tamondojo@gmail.com>
2018-11-20 16:41:08 +0900

    style: Remove Vuetify

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+// import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
-Vue.use(Vuetify)
+// Vue.use(Vuetify)
 
 window.eventBus = new Vue()
 
